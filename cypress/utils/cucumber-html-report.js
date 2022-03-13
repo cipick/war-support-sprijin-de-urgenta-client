7bc38fb49fe39743f90176f8919a18dcb4bf8e5b
Ciprian Platica <platica.ciprian@gmail.com>
2022-03-13 21:25:03 +0200

    Update reporter config

--- cypress/utils/cucumber-html-report.js
+++ cypress/utils/cucumber-html-report.js
@@ -3,4 +3,7 @@ const report = require('multiple-cucumber-html-reporter')
 report.generate({
   jsonDir: 'cypress/cucumber-json',
   reportPath: './reports/cucumber',
+  hideMetadata: true,
+  pageTitle: 'Sprijin De Urgenta E2E tests results | Code4ro',
+  reportName: 'Sprijin De Urgenta',
 })
